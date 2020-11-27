9b3f00f87b315b577d52590b853fd3ca5af9d35b
Vijendra-Achar <vijendra.1995@gmail.com>
2020-11-27 09:22:25 +0530

    Minor changes and bug fixes ⚒

--- src/app/home/discover/discover.page.ts
+++ src/app/home/discover/discover.page.ts
@@ -9,16 +9,15 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit, OnDestroy {
-
   thePlaces: Place[];
   placesSubscription: Subscription;
 
-  constructor(private homeServeice: HomeService) { }
+  constructor(private homeServeice: HomeService) {}
 
   ngOnInit() {
-    this.placesSubscription = this.homeServeice.getPlaces.subscribe(places => {
+    this.placesSubscription = this.homeServeice.getPlaces.subscribe((places) => {
       this.thePlaces = places;
-    })
+    });
   }
 
   ngOnDestroy() {
--- src/app/services/place.model.ts
+++ src/app/services/place.model.ts
@@ -1,15 +1,15 @@
 export class Place {
-    constructor(
-        public id: string,
-        public placeName: string,
-        public description: string,
-        public address: string,
-        public imageUrl: string,
-        public price: number,
-        public rating: number,
-        public features: string[],
-        public availableFrom: Date,
-        public availableTill: Date,
-        public userId: string
-    ) { }
+  constructor(
+    public id: string,
+    public placeName: string,
+    public description: string,
+    public address: string,
+    public imageUrl: string,
+    public price: number,
+    public rating: number,
+    public features: string[],
+    public availableFrom: Date,
+    public availableTill: Date,
+    public userId: string,
+  ) {}
 }
